9d6ea7f5f7cb99173f62f3b0c8cca67f2684aad9
Erik Slagter <erik@openpli.org>
2014-04-05 10:28:08 +0200

    Add rc script.

---
+++ src/streamproxy.sh
@@ -0,0 +1,37 @@
+### BEGIN INIT INFO
+# Provides:				streamproxy
+# Required-Start:		$syslog $networking
+# Required-Stop:		$syslog
+# Default-Start:		2 3 4 5
+# Default-Stop:			1
+# Short-Description:	Streamproxy for streaming and transcoding live and files.
+### END INIT INFO
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+DAEMON=/usr/bin/streamproxy
+NAME=streamproxy
+DESC="Streamproxy"
+
+test -x "$DAEMON" || exit 0
+
+case "$1" in
+
+	start)
+		echo -n "Starting ${DESC}: "
+		start-stop-daemon -S -x "$DAEMON" -- -l 8002:transcode -l 8003:transcode
+		echo "${NAME}."
+		;;
+
+	stop)
+		echo -n "Stopping ${DESC}: "
+		start-stop-daemon -K -x "$DAEMON"
+		echo "${NAME}."
+		;;
+
+	*)
+		echo "usage: /etc/init.d/${NAME} {start|stop}" >&2
+		exit 1
+		;;
+esac
+
+exit 0
