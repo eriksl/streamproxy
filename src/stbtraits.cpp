fcbd9531dbe7eb68abc0c797448f0dcebeeb0268
Erik Slagter <erik@openpli.org>
2018-11-24 13:25:16 +0100

    Cosmetic: re-arrange machines, order by transcoding API type.

--- src/stbtraits.cpp
+++ src/stbtraits.cpp
@@ -730,6 +730,153 @@ static const stb_feature_t features_galaxy4k[] =
 	},
 };
 
+static const stb_feature_t features_lunix34k[] =
+{
+	{
+		.type			= stb_traits_type_string_enum,
+		.description	= "Video codec",
+		.id				= "vcodec",
+		.settable		= false,
+		.api_data		= "video_codec",
+		.value			=
+		{
+			.string_enum_type =
+			{
+				.default_value	= "h264",
+				.enum_values	=
+				{
+					"h264", 0,
+				},
+			},
+		},
+	},
+	{
+		.type			= stb_traits_type_string_enum,
+		.description	= "Audio codec",
+		.id				= "acodec",
+		.settable		= false,
+		.api_data		= "audio_codec",
+		.value			=
+		{
+			.string_enum_type =
+			{
+				.default_value	= "aac",
+				.enum_values	=
+				{
+					"aac", 0,
+				},
+			},
+		},
+	},
+	{
+		.type			= stb_traits_type_int,
+		.description	= "Total stream bit rate",
+		.id				= "bitrate",
+		.settable		= true,
+		.api_data		= "bitrate",
+		.value			=
+		{
+			.int_type =
+			{
+				.default_value	= 1000,
+				.min_value		= 100,
+				.max_value		= 10000,
+				.scaling_factor	= 1000,
+			},
+		},
+	},
+	{
+		.type			= stb_traits_type_string_enum,
+		.description	= "Picture dimensions",
+		.id				= "size",
+		.settable		= true,
+		.api_data		= "display_format",
+		.value			=
+		{
+			.string_enum_type =
+			{
+				.default_value	= "480p",
+				.enum_values	=
+				{
+					"480p", "576p", "720p", 0,
+				},
+			},
+		},
+	},
+	{
+		.type			= stb_traits_type_string_enum,
+		.description	= "Video codec profile",
+		.id				= "profile",
+		.settable		= true,
+		.api_data		= "profile",
+		.value			=
+		{
+			.string_enum_type =
+			{
+				.default_value	= "baseline",
+				.enum_values	=
+				{
+					"baseline", "simple", "main", "high", 0,
+				},
+			},
+		},
+	},
+	{
+		.type			= stb_traits_type_string_enum,
+		.description	= "Video codec level",
+		.id				= "level",
+		.settable		= true,
+		.api_data		= "level",
+		.value			=
+		{
+			.string_enum_type =
+			{
+				.default_value	= "3.1",
+				.enum_values	=
+				{
+					"3.0", "3.1", "3.2", "4.0", "4.1",
+				},
+			},
+		},
+	},
+	{
+		.type			= stb_traits_type_int,
+		.description	= "Max. bidirectional frames in a row",
+		.id				= "bframes",
+		.settable		= true,
+		.api_data		= "gop_frameb",
+		.value			=
+		{
+			.int_type =
+			{
+				.default_value	= 2,
+				.min_value		= 0,
+				.max_value		= 2,
+				.scaling_factor	= 1,
+			},
+		},
+	},
+	{
+		.type			= stb_traits_type_string_enum,
+		.description	= "Frame rate",
+		.id				= "framerate",
+		.settable		= true,
+		.api_data		= "framerate",
+		.value			=
+		{
+			.string_enum_type =
+			{
+				.default_value	= "30000",
+				.enum_values	=
+				{
+					"24000", "25000", "30000", 0,
+				},
+			},
+		},
+	},
+};
+
+
 static const stb_feature_t features_et10000[] =
 {
 	{
@@ -874,152 +1021,6 @@ static const stb_feature_t features_hd2400[] =
 				.enum_values	=
 				{
 					"480p", "576p", "720p", 0,
-				},
-			},
-		},
-	},
-};
-
-static const stb_feature_t features_lunix34k[] =
-{
-	{
-		.type			= stb_traits_type_string_enum,
-		.description	= "Video codec",
-		.id				= "vcodec",
-		.settable		= false,
-		.api_data		= "video_codec",
-		.value			=
-		{
-			.string_enum_type =
-			{
-				.default_value	= "h264",
-				.enum_values	=
-				{
-					"h264", 0,
-				},
-			},
-		},
-	},
-	{
-		.type			= stb_traits_type_string_enum,
-		.description	= "Audio codec",
-		.id				= "acodec",
-		.settable		= false,
-		.api_data		= "audio_codec",
-		.value			=
-		{
-			.string_enum_type =
-			{
-				.default_value	= "aac",
-				.enum_values	=
-				{
-					"aac", 0,
-				},
-			},
-		},
-	},
-	{
-		.type			= stb_traits_type_int,
-		.description	= "Total stream bit rate",
-		.id				= "bitrate",
-		.settable		= true,
-		.api_data		= "bitrate",
-		.value			=
-		{
-			.int_type =
-			{
-				.default_value	= 1000,
-				.min_value		= 100,
-				.max_value		= 10000,
-				.scaling_factor	= 1000,
-			},
-		},
-	},
-	{
-		.type			= stb_traits_type_string_enum,
-		.description	= "Picture dimensions",
-		.id				= "size",
-		.settable		= true,
-		.api_data		= "display_format",
-		.value			=
-		{
-			.string_enum_type =
-			{
-				.default_value	= "480p",
-				.enum_values	=
-				{
-					"480p", "576p", "720p", 0,
-				},
-			},
-		},
-	},
-	{
-		.type			= stb_traits_type_string_enum,
-		.description	= "Video codec profile",
-		.id				= "profile",
-		.settable		= true,
-		.api_data		= "profile",
-		.value			=
-		{
-			.string_enum_type =
-			{
-				.default_value	= "baseline",
-				.enum_values	=
-				{
-					"baseline", "simple", "main", "high", 0,
-				},
-			},
-		},
-	},
-	{
-		.type			= stb_traits_type_string_enum,
-		.description	= "Video codec level",
-		.id				= "level",
-		.settable		= true,
-		.api_data		= "level",
-		.value			=
-		{
-			.string_enum_type =
-			{
-				.default_value	= "3.1",
-				.enum_values	=
-				{
-					"3.0", "3.1", "3.2", "4.0", "4.1",
-				},
-			},
-		},
-	},
-	{
-		.type			= stb_traits_type_int,
-		.description	= "Max. bidirectional frames in a row",
-		.id				= "bframes",
-		.settable		= true,
-		.api_data		= "gop_frameb",
-		.value			=
-		{
-			.int_type =
-			{
-				.default_value	= 2,
-				.min_value		= 0,
-				.max_value		= 2,
-				.scaling_factor	= 1,
-			},
-		},
-	},
-	{
-		.type			= stb_traits_type_string_enum,
-		.description	= "Frame rate",
-		.id				= "framerate",
-		.settable		= true,
-		.api_data		= "framerate",
-		.value			=
-		{
-			.string_enum_type =
-			{
-				.default_value	= "30000",
-				.enum_values	=
-				{
-					"24000", "25000", "30000", 0,
 				},
 			},
 		},
@@ -1222,22 +1223,6 @@ const stbs_traits_t stbs_traits =
 			.features			= features_vuarm,
 		},
 		{
-			.manufacturer		= "XSarius",
-			.model				= "Galaxy4k",
-			.chipset			= "bcm7252s",
-			.transcoding_type	= stb_transcoding_broadcom,
-			.quirks				= (stb_quirks_t)0,
-			.encoders			= 1,
-			.num_id				= 2,
-			.id					=
-			{
-				{ "/proc/stb/info/chipset", "7252S" },
-				{ "/proc/stb/info/hwmodel", "galaxy4k" },
-			},
-			.num_features		= 8,
-			.features			= features_galaxy4k,
-		},
-		{
 			.manufacturer		= "GigaBlue",
 			.model				= "UE 4k",
 			.chipset			= "bcm7252s",
@@ -1270,6 +1255,38 @@ const stbs_traits_t stbs_traits =
 			.features			= features_gb4k,
 		},
 		{
+			.manufacturer		= "XSarius",
+			.model				= "Galaxy4k",
+			.chipset			= "bcm7252s",
+			.transcoding_type	= stb_transcoding_broadcom,
+			.quirks				= (stb_quirks_t)0,
+			.encoders			= 1,
+			.num_id				= 2,
+			.id					=
+			{
+				{ "/proc/stb/info/chipset", "7252S" },
+				{ "/proc/stb/info/hwmodel", "galaxy4k" },
+			},
+			.num_features		= 8,
+			.features			= features_galaxy4k,
+		},
+		{
+			.manufacturer		= "Qviart",
+			.model				= "Lunix3-4k",
+			.chipset			= "bcm7252s",
+			.transcoding_type	= stb_transcoding_broadcom,
+			.quirks				= (stb_quirks_t)0,
+			.encoders			= 1,
+			.num_id				= 2,
+			.id					=
+			{
+				{ "/proc/stb/info/chipset", "7252S" },
+				{ "/proc/stb/info/hwmodel", "lunix3-4k" },
+			},
+			.num_features		= 8,
+			.features			= features_lunix34k,
+		},
+		{
 			.manufacturer		= "XTrend",
 			.model				= "ET10000",
 			.chipset			= "bcm7424",
@@ -1299,22 +1316,6 @@ const stbs_traits_t stbs_traits =
 			},
 			.num_features		= 4,
 			.features			= features_hd2400,
-		},
-		{
-			.manufacturer		= "Qviart",
-			.model				= "Lunix3-4k",
-			.chipset			= "bcm7252s",
-			.transcoding_type	= stb_transcoding_broadcom,
-			.quirks				= (stb_quirks_t)0,
-			.encoders			= 1,
-			.num_id				= 2,
-			.id					=
-			{
-				{ "/proc/stb/info/chipset", "7252S" },
-				{ "/proc/stb/info/hwmodel", "lunix3-4k" },
-			},
-			.num_features		= 8,
-			.features			= features_lunix34k,
 		},
 		{
 			.manufacturer		= "Zgemma",
